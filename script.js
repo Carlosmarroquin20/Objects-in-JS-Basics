28bbf19a22ec18836eb83d1cb579b6d37e84b351
Emanuel Marroquin <carlosema322@gmail.com>
2024-12-02 20:55:56 -0600

    Add basic objects person1 and person2 with properties and log their values

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello from Js")
+
+const person1 = {
+    firstName: 'Ema',
+    lastName: "Marroquin",
+    age: 21,
+    isEmployed: true,
+    
+}
+
+const person2 = {
+    firstName: 'Diego',
+    lastName: "Carra",
+    age: 33,
+    isEmployed: false,
+}
+
+console.log(person1.firstName)
+console.log(person1.lastName)
+console.log(person1.age)
+console.log(person1.isEmployed)
+
+console.log(person2.firstName)
+console.log(person2.lastName)
+console.log(person2.age)
+console.log(person2.isEmployed)
